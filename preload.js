329cf6834d7d38106e5dd46df0f77ca0a273cff7
bartkyy <ytbarto12@gmail.com>
2026-05-15 21:52:21 +0200

    Update v1.0.1

--- preload.js
+++ preload.js
@@ -1,9 +1,13 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
-  readDir: (path) => ipcRenderer.invoke('read-dir', path),
-  openDialog: () => ipcRenderer.invoke('dialog:openFile'),
-  openDirectoryDialog: () => ipcRenderer.invoke('dialog:openDirectory'),
-  openPath: (path) => ipcRenderer.invoke('file:openPath', path),
-  saveFile: (data) => ipcRenderer.invoke('file:save', data)
+  readDir: (path) => ipcRenderer.invoke('dir:read', path),
+  openPath: (path) => ipcRenderer.invoke('file:open', path),
+  openDialog: () => ipcRenderer.invoke('dialog:open-file'),
+  openDirectoryDialog: () => ipcRenderer.invoke('dialog:open-dir'),
+  saveFile: (data) => ipcRenderer.invoke('file:save', data),
+
+  onMenuOpen: (callback) => ipcRenderer.on('menu:open-file', () => callback()),
+  onMenuOpenFolder: (callback) => ipcRenderer.on('menu:open-folder', () => callback()),
+  onMenuSave: (callback) => ipcRenderer.on('menu:save-file', () => callback())
 });
